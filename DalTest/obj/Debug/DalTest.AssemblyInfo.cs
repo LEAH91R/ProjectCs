a7bd25b89bd7cd83812cc180a409d39d0f33d1eb
dh0556743432-ctrl <dh0556743432@gmail.com>
2026-02-15 19:00:55 +0200

    finish level6

--- DalTest/obj/Debug/DalTest.AssemblyInfo.cs
+++ DalTest/obj/Debug/DalTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DalTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c3aa4e83d5e74f8c68996a815c0a8d7beaaf3fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+163307b39b092baf4de21df444c63207cb734cb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DalTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DalTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
