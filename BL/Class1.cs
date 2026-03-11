decf440979dd10988d893ffd60a6ee30dd782b3f
tamaR-797 <3295797@gmail.com>
2026-03-11 19:36:11 +0200

    aaa

--- BL/Class1.cs
+++ BL/Class1.cs
@@ -1,7 +1,20 @@
+<<<<<<< Updated upstream
 ﻿namespace BL
 {
     public class Class1
     {
 
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Class1
+    {
+>>>>>>> Stashed changes
     }
 }
