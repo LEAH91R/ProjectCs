ffa2e72c1c93bfe4ea5be7278df4138e82c7538d
tamaR-797 <3295797@gmail.com>
2026-03-17 15:18:36 +0200

    start level9

---
+++ DalXml/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalXml
+{
+    internal static class Config
+    {
+        private static string FileName = "data-config";
+        internal static int getStaticValueSale { get => ++staticValueSale };
+        internal static int getStaticValueCustomer { get => ++staticValueCustomer };
+        internal static int getStaticValueProduct { get => ++staticValueProduct };
+    } 
+}
