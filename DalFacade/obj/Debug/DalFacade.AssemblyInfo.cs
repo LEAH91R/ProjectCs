4a4e487acd76941026b550057fc200d274a14384
tamaR-797 <3295797@gmail.com>
2026-05-10 19:45:34 +0300

    ??????

--- DalFacade/obj/Debug/DalFacade.AssemblyInfo.cs
+++ DalFacade/obj/Debug/DalFacade.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DalFacade")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9276923b3ea3f9a4504e4a68b367f14f79d7731")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b9853c95f16e66ea8ce847f11f70d87f4c21ee2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DalFacade")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DalFacade")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
